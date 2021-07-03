222268900faaf692180a6d606c5b7abac8a4510d
Gautam Jha <gautamjha.mailbox@gmail.com>
2021-07-03 23:08:26 +0530

    GTM

--- components/Layout.js
+++ components/Layout.js
@@ -13,6 +13,15 @@ export default function Layout({ children }) {
                     name="google-site-verification"
                     content="ytluGmwzLOUZL24jkQBlizLfKSvv756kraKONPFP4es"
                 />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','G-392E7F5M2E');`
+                    }}
+                />
             </Head>
 
             <Navbar />
