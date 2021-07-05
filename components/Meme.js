d38676a978e5766f00a4d38eb0a0ee68466fd1ee
Gautam Jha <gautamjha.mailbox@gmail.com>
2021-07-05 22:15:08 +0530

    API without next image

--- components/Meme.js
+++ components/Meme.js
@@ -12,7 +12,7 @@ import {
     // WhatsappShareButton,
     // WhatsappIcon
 } from 'next-share';
-import Image from 'next/image';
+// import Image from 'next/image';
 import Loader from './Loader';
 import styles from '../styles/Meme.module.css';
 
@@ -22,7 +22,7 @@ function Meme(props) {
     return data && !loading ? (
         <div
             className={`${styles.meme} rounded overflow-hidden shadow-lg bg-white dark:bg-white dark:bg-opacity-5 dark:border-black dark:hover:border-yellow-300 mt-5`}>
-            <Image
+            <img
                 className="w-full lazy"
                 src={`/api/img/${encodeURIComponent(data.url)}`}
                 alt={data.title}
