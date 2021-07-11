45964d21a3a07c35544bcf2e1f059e8871ff626a
GAUTAM JHA <gautamjha.mailbox@gmail.com>
2021-07-12 01:45:50 +0530

    Research links removed

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -15,7 +15,7 @@ function Sidebar({ classname }) {
         comedyheaven: 'Funny',
         animememes: 'Anime',
         // hentai: 'Hentai', // research purposes only
-        nsfw: 'NSFW',
+        //nsfw: 'NSFW',
         comic: 'Comic'
     };
 
