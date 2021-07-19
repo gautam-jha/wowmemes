ab86c1c88356735cd65c9b613779b882f98cdd43
Gautam Jha <gautamjha.mailbox@gmail.com>
2021-07-20 01:34:01 +0530

    Removed Analytics

--- components/Layout.js
+++ components/Layout.js
@@ -16,15 +16,7 @@ export default function Layout({ children, featured }) {
                     content="ytluGmwzLOUZL24jkQBlizLfKSvv756kraKONPFP4es"
                 />
                 {/* eslint-disable */}
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','G-392E7F5M2E');`
-                    }}
-                />
+
                 {/* eslint-enable */}
             </Head>
 
