33ce15ce0b72079b0b1e0e45ea6fe47edda11d2a
gautam-jha <gautamjha.mailbox@gmail.com>
2021-08-18 19:30:49 +0000

    Deploying to gh-pages from @ gautam-jha/memehub@2f7eaa4b632eb1261b8f8ec554a16915317d896a 🚀

--- components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Footer } from './Footer';
-export { default as Header } from './Header';
-export { default as Navbar } from './Navbar';
-export { default as Meme } from './Meme';
-export { default as Sidebar } from './Sidebar';
-export { default as Featured } from './Featured';
--- pages/api/img/index.js
+++
@@ -1,7 +0,0 @@
-// const request = require('request');
-
-function handler(req, res) {
-    res.status(500).json('Unauthorized!');
-}
-
-export default handler;
